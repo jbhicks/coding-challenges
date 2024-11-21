fce0928d15c6ab7fd3401689a1a068a856ad23d4
Josh Hicks <joshua.brock.hicks@gmail.com>
2024-11-21 10:53:12 -0700

    updated utils

---
+++ 2-json-parser/go.mod
@@ -0,0 +1,3 @@
+module github.com/jbhicks/coding-challenges-go/2-json-parser
+
+go 1.23.2
--- util/util.go
+++ util/util.go
@@ -3,6 +3,8 @@ package util
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 )
 
 // PrettyPrint takes an interface{} and prints it in a pretty format.
@@ -12,4 +14,20 @@ func PrettyPrint(v interface{}) {
 		fmt.Println("error:", err)
 	}
 	fmt.Println(string(b))
+}
+
+func GetFileContent(filePath string) string {
+	data, err := os.ReadFile(filePath)
+	Check(err, false)
+	return string(data)
+}
+
+// Reusable error checking function
+func Check(e error, atTheDisco bool) {
+	if e != nil {
+		log.Fatal(e)
+		if atTheDisco {
+			panic(e)
+		}
+	}
 }
